d396f139e26dc1cd2d393714d97e83c02262ce54
Erica Gurung <egurung@student.unimelb.edu.au>
2023-05-24 00:48:23 +1000

    Home page and footer

--- script.js
+++ script.js
@@ -13,35 +13,47 @@ menuItems.forEach((menuItem) => {
     });
 });
 
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// Slideshow functionality
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    // Set the default picture to the middle
+    let active = 3;
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("sliderImg");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  } 
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+        // Create blur effect on background images
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt})`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(3px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt})`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(3px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+
+    // Call function when arrow is clicked
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }
