03fdb22ccd89f88bdd4aed35d0a1472f019fe492
Erica Gurung <egurung@student.unimelb.edu.au>
2023-05-24 15:48:29 +1000

    Responsive top half

--- script.js
+++ script.js
@@ -16,36 +16,36 @@ menuItems.forEach((menuItem) => {
 
 // Slideshow functionality
 let items = document.querySelectorAll('.slider .item');
-    let next = document.getElementById('next');
-    let prev = document.getElementById('prev');
-    
-    // Set the default picture to the middle
-    let active = 3;
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
 
-    function loadShow(){
-        let stt = 0;
-        items[active].style.transform = `none`;
-        items[active].style.zIndex = 1;
-        items[active].style.filter = 'none';
-        items[active].style.opacity = 1;
+// Set the default picture to the middle
+let active = 3;
 
-        // Create blur effect on background images
-        for(var i = active + 1; i < items.length; i++){
-            stt++;
-            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt})`;
-            items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(3px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0.6;
-        }
-        stt = 0;
-        for(var i = active - 1; i >= 0; i--){
-            stt++;
-            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt})`;
-            items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(3px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0.6;
-        }
+function loadShow(){
+    let stt = 0;
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+
+    // Create blur effect on background images
+    for(var i = active + 1; i < items.length; i++){
+        stt++;
+        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt})`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(3px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
     }
+    stt = 0;
+    for(var i = active - 1; i >= 0; i--){
+        stt++;
+        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt})`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(3px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
 
     // Call function when arrow is clicked
     loadShow();
